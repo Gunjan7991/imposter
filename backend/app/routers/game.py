a885578f428714aa37bf37d711c2e58e611b15e1
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2024-03-20 22:30:13 -0500

    Game mechanism updated

--- backend/app/routers/game.py
+++ backend/app/routers/game.py
@@ -3,23 +3,31 @@ from ..settings import *
 from .. import schemas
 import random
 
-router = APIRouter(prefix="/api/v1", tags=["Authentication"])
+router = APIRouter()
+
+@router.websocket('/create_room')
+def create_room_route(websocket: WebSocket, request: schemas.create_room):
+    owner = (websocket, request.owner_name)
+    room_number = create_room(owner, request.imposters)
+    join_room(room_number, owner)
+    return ({'room_number': room_number})
 
 
-@router.put('/create_room', response_model=None)
-def create_room_route(request: schemas.imposter_in, response_model=None):
-    room_id = create_room(request.num_of_imposter)
-    return ({'room_id': room_id})
-
-
-@router.put('/join_game')
-def join_game(request: schemas.player):
-    if request.verify in temporary_rooms:
-        join_room(request.verify, request.name)
+@router.websocket('/join_game')
+def join_game(websocket: WebSocket, request: schemas.player):
+    player = (websocket, request.player_name)
+    if request.room_number in temporary_rooms:
+        join_room(request.room_number, player)
 
 
 @router.websocket("/start/{room_number}")
 def start_game(room_number):
     if room_number in temporary_rooms:
-        imposters = random.sample(
-            temporary_rooms[room_number]['players'], temporary_rooms[room_number]['num_of_imposters'])
+        room: Room = temporary_rooms[room_number]
+        imposters = random.sample(room.players, room.imposters)
+        for player in room.players:
+            if player not in imposters:
+                room.manager.send_personal_message(f"The location is {random.sample(places,1)}.", player[0])
+            else:
+                room.manager.send_personal_message(f"You are the imposter!! Try to blend in.", player[0])
+            
